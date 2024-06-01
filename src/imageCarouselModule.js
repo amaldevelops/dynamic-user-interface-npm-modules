80f9d12f4993ef63f5264e3358e4379d49a69cf5
Amal K <12756149+1Amal@users.noreply.github.com>
2024-06-01 15:18:23 +1000

    Added code for the circle button navigation

--- src/imageCarouselModule.js
+++ src/imageCarouselModule.js
@@ -2,9 +2,9 @@
 
 export class imageCarousel {
   constructor() {
-    this.imageDiv1 = document.querySelector(".imageCarouselImagesDiv");
-    this.imageDiv2 = document.querySelector(".imageCarouselImagesDiv");
-    this.imageDiv3 = document.querySelector(".imageCarouselImagesDiv");
+    this.imageDiv1 = document.querySelector(".imageDiv1");
+    this.imageDiv2 = document.querySelector(".imageDiv2");
+    this.imageDiv3 = document.querySelector(".imageDiv3");
   }
 
   userInput() {
@@ -43,22 +43,28 @@ export class imageCarousel {
   }
 
   moveLeft() {
-    this.imageDiv1.style["grid-area"] = "2/1/3/3";
+    this.imageCarouselImagesDiv.style["grid-area"] = "2/1/3/4";
   }
 
   moveRight() {
-    this.imageDiv1.style["grid-area"] = "2/2/3/4";
+    this.imageCarouselImagesDiv.style["grid-area"] = "2/2/3/4";
   }
 
   circleButton1Clicked() {
-    this.imageDiv1.style["grid-area"] = "2/2/3/3";
+    this.imageDiv1.style["grid-area"] = "2/1/3/2";
+    this.imageDiv2.style["grid-area"] = "2/2/3/3";
+    this.imageDiv3.style["grid-area"] = "2/3/3/4";
   }
 
   circleButton2Clicked() {
-    this.imageDiv1.style["grid-area"] = "2/1/3/2";
+    this.imageDiv1.style["grid-area"] = "2/2/3/3";
+    this.imageDiv2.style["grid-area"] = "2/1/3/2";
+    this.imageDiv3.style["grid-area"] = "2/3/3/4";
   }
 
   circleButton3Clicked() {
     this.imageDiv1.style["grid-area"] = "2/3/3/4";
+    this.imageDiv2.style["grid-area"] = "2/2/3/3";
+    this.imageDiv3.style["grid-area"] = "2/1/3/2" ;
   }
 }
